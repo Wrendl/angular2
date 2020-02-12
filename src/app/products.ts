36be5796353c0720f4b303c9a3571f307cc8d61b
Wrendl <narimanganiyev26@gmail.com>
2020-02-12 16:09:40 +0600

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,24 +1,61 @@
 export interface Product{
   name: string,
-  price: number,
-  description: string
+  rating: number,
+  description: string,
+  ll: string
 }
 
 export const products = [
   {
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'Asus ROG STRIX-GTX1050TI-4G-GAMING',
+    rating: 5.0,
+    description: 'Бренд: ASUS Модель графического процессора: GeForce GTX 1050 Ti Состояние товара: Бывший в употреблении Тип охладителя: Вентилятор Производитель чипсета: NVIDIA Тип выходного интерфейса: DVI Тип выходного интерфейса: HDMI',
+    ll: ""
   },
   {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'PALIT PCIE16 GT710 2GB GDDR3 PA-GT710-2GD3H',
+    rating: 5.0,
+    description: 'Бренд: Palit Модель графического процессора: GeForce GT 710 Объем видеопамяти: 2 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express Тип охладителя: Вентилятор Тип видеопамяти: GDDR3 Производитель чипсета: NVIDIA'
   },
   {
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'Yeston Radeon RX 550 GPU 4 ГБ GDDR5 128 бит',
+    rating: 4.9,
+    description: 'Бренд: yeston Модель графического процессора: Radeon RX550 Объем видеопамяти: 4 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express Тип охладителя: Вентилятор Тип видеопамяти: GDDR5'
+  },
+  {
+    name: 'Видеокарта RX 570 DirectX 12 8GB 256-Bit GDDR5',
+    rating: 4.8,
+    description: 'Бренд: VEINEDA Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16 Модель графического процессора: Radeon RX570 Объем видеопамяти: 8 Гб Тип охладителя: Вентилятор Тип видеопамяти: GDDR5'
+  },
+  {
+    name: 'Видеокарта Gigabyte Gaming nVidia GeForce GTX 1660 Super',
+    rating: 4.9,
+    description: 'Бренд: Gigabyte Модель графического процессора: GTX 1660 Объем видеопамяти: 6 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16'
+  },
+  {
+    name: 'Yeston Radeon RX 580 GPU 8 ГБ GDDR5 256bit',
+    rating: 4.6,
+    description: 'Бренд: yeston Модель графического процессора: Radeon RX580 Объем видеопамяти: 8 Гб Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16'
+  },
+  {
+    name: 'Видеокарта ASUS ROG nVidia GeForce GTX 1050 Ti ',
+    rating: 5.0,
+    description: 'Бренд: Asus Модель графического процессора: GeForce GTX 1050 Ti Объем видеопамяти: 4 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16'
+  },
+  {
+    name: 'Yeston Radeon mini RX 550 GPU 4 ГБ GDDR5 128 бит',
+    rating: 4.7,
+    description: 'Бренд: yeston Модель графического процессора: Radeon RX550 Объем видеопамяти: 4 ГБ Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16'
+  },
+  {
+    name: 'Yeston RX550--4G D5 Видеокарты Radeon Chill 4 Гб',
+    rating: 5.0,
+    description: 'Бренд: yeston Состояние товара: Новый Тип интерфейса: Компьютерная шина PCI Express 3.0 X16 Модель графического процессора: Radeon RX550 Объем видеопамяти: 4 ГБТип охладителя: Вентилятор Тип видеопамяти: GDDR5'
+  },
+  {
+    name: 'SAPPHIRE RX 560 4 Гб видеокарта GPU Radeon RX 560D 4G ',
+    rating: 4.8,
+    description: 'Бренд: SAPPHIRE Состояние товара: Бывший в употреблении Тип интерфейса: Компьютерная шина PCI Express 3.0 X16 Объем видеопамяти: 4 ГБ Модель графического процессора: Radeon RX560'
   }
 ];
 
